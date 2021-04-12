93c1782cacd7cf4f083414108665061a0b370407
Nicolas <nicolas.valenchon@polymtl.ca>
2021-04-11 21:32:12 -0400

    search saved in txt

--- TP3/MOT17_dataset_tracker_particle.py
+++ TP3/MOT17_dataset_tracker_particle.py
@@ -26,25 +26,20 @@ def track_MOT(scale=0.25, box_size_evolution=1, particle_movement=20, nb_particl
     return accuracy
 
 
-param_grid = {'scale': np.arange(0.1, 0.5, step=0.05), 'box_size_evolution': range(1, 20, 2),
-              'particle_movement': range(1, 50, 2), 'nb_particles': np.append(range(1, 100, 10), range(100, 500, 50))}
+param_grid = {'scale': np.arange(0.1, 0.5, step=0.05), 'box_size_evolution': range(1, 16, 2),
+              'particle_movement': range(1, 53, 3), 'nb_particles': np.append(range(1, 100, 10), range(100, 500, 50))}
 
-for epoch in range(2):
+for epoch in range(10):
     print(f"Epoch {epoch}")
-    scale = np.random.choice(param_grid["scale"])
+    scale = 0.25  # np.random.choice(param_grid["scale"])
     box_size_evolution = np.random.choice(param_grid["box_size_evolution"])
     particle_movement = np.random.choice(param_grid["particle_movement"])
     nb_particles = np.random.choice(param_grid["nb_particles"])
     print(f"Parameters : scale = {scale}, box_size_evolution = {box_size_evolution}, "
           f"particle_movement = {particle_movement}, nb_particles = {nb_particles}")
-    mean_score = 0
-    for k in range(3):
-        score = track_MOT(scale=scale, box_size_evolution=box_size_evolution, particle_movement=particle_movement, nb_particles=nb_particles)
-        if score > 0.2:
-            print(f"Run {k} : score = {score}")
-            mean_score += score
-        else:
-            mean_score = 0
-            break
-    mean_score /= 3
-    print(f"Mean score = {mean_score}")
+    score = track_MOT(scale=scale, box_size_evolution=box_size_evolution, particle_movement=particle_movement, nb_particles=nb_particles)
+    print(f"Score = {score}")
+    with open("score.txt", "a") as file:
+        file.write(f"scale = {scale}; box_size_evolution = {box_size_evolution};particle_movement = {particle_movement}; nb_particles = {nb_particles};Score = {score}\n")
+
+
